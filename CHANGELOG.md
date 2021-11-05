ce40f2f4a1f1a9e20ff829e2cf5b646eeb919644
John Shaffer <jshaffer2112@protonmail.com>
2021-11-05 12:02:58 -0500

    Add Nix and direnv files for development environment

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 ## Unreleased
 * Update to clojure 1.11.0-alpha2 and cheshire 5.10.1
+* Add [Nix](https://nixos.org/) and [direnv](https://direnv.net/) files for development environment
 
 ## 0.1.0 (2021-05-09)
 
---
+++ default.nix
@@ -0,0 +1,15 @@
+let
+  rev = "5f244caea76105b63d826911b2a1563d33ff1cdc";
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = "1xlgynfw9svy7nvh9nkxsxdzncv9hg99gbvbwv3gmrhmzc3sar75";
+  };
+  pkgs = import nixpkgs {};
+in with pkgs;
+mkShell {
+  buildInputs = [
+    clojure
+    glibcLocales # For rlwrap locale
+    rlwrap
+  ];
+}
