ec7f7537b8c14553b35966f863f1f4172de838d5
John Shaffer <jdsha@proton.me>
2022-09-12 18:11:15 -0500

    Automate builds and releases

--- README.md
+++ README.md
@@ -3,15 +3,3 @@
 [![Clojars Project](https://img.shields.io/clojars/v/io.staticweb/cloudformation-templating.svg)](https://clojars.org/io.staticweb/cloudformation-templating)
 
 A library for creating CloudFormation templates.
-
-## Install
-
-Add to deps.edn:
-```edn
-io.staticweb/cloudformation-templating {:mvn/version "0.1.0"}
-```
-
-Or add to project.clj:
-```edn
-[io.staticweb/cloudformation-templating "0.1.0"]
-```
---
+++ build.clj
@@ -0,0 +1,19 @@
+(ns build
+  (:require [org.corfield.build :as bb]))
+
+(def lib 'io.staticweb/cloudformation-templating)
+(def version "1.0.0")
+(defn get-version [opts]
+  (str version (when (:snapshot opts) "-SNAPSHOT")))
+
+(defn jar "Build the JAR." [opts]
+  (-> opts
+      (assoc :lib lib :version (get-version opts))
+      bb/clean
+      (assoc :src-pom "template/pom.xml")
+      bb/jar))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version (get-version opts))
+      bb/deploy))
