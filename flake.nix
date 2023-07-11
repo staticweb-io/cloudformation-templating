c990098692185c1082b239852a922a30991e3e53
John Shaffer <jdsha@proton.me>
2023-07-10 23:33:54 -0500

    Convert dev shell to nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "cloudformation-templating";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          buildInputs = [
+            clojure
+            jdk
+            rlwrap # Used by clj
+            time
+          ];
+        };
+      });
+}
--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
-with pkgs;
-mkShell {
-  buildInputs = [
-    (clojure.override { jdk = openjdk17; })
-    glibcLocales # For rlwrap locale
-    rlwrap
-  ];
-}
