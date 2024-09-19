be6771ef9abeb1c457ca82199ae08356d5932bfa
John Shaffer <jdsha@pm.me>
2024-09-19 10:57:02 -0500

    Upgrade deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "cloudformation-templating";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
